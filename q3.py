60ca4d52cc0d6cfa659fb0f0d36aefece06edbb9
Nitik Sharma <sharmanitik36@gmail.com>
2025-08-25 19:28:58 +0530

    do 3rd part

---
+++ q3.py
@@ -0,0 +1,13 @@
+# 19. A user enters a sentence. Print a new string that contains only the even-indexed
+# characters of the original sentence.
+# ● Example: "python" → "pto"
+
+
+# strr=input("Enter a string : ")
+
+# i=0
+# while i<len(strr):
+#     if i%2==0:
+#         print(strr[i],end=" ")
+#     i=i+1
+
