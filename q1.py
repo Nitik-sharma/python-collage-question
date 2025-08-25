0d64ba10a9afab5224eccd60c2d4689a3946395c
Nitik Sharma <sharmanitik36@gmail.com>
2025-08-25 19:04:07 +0530

    do first 10 question

---
+++ q1.py
@@ -0,0 +1,84 @@
+
+# 1.Take a string input from the user and print its first character.
+
+# strr=input("Enter a String : ")
+
+# print(strr[0])
+
+#2. Take a string input and print the last character using slicing
+
+# strr=input("Enter a String : ")
+
+# print(strr[-1::])
+
+
+#3. Input a string and print the first three characters.
+
+# strr=input("Enter a String : ")
+
+# print(strr[0:3])
+
+# 4.Input a string and print the string in reverse using slicing.
+
+# strr=input("Enter a Striing : ")
+# print(strr[::-1])
+
+#5.
+# 5.Input a string and print every second character using slicing.
+
+# strr=input("Enter a String : ")
+# print(strr[1::2])
+
+#6.Input two numbers and print the larger one using if-else.
+
+# num1=int(input("Enter a number : "))
+# num2=int(input("Enter a number : "))
+
+# if num1>num2:
+#     print("Num1 is greater ")
+# else:
+#     print("Num2 is greater ")
+
+
+# 7. Input a number and check if it is even or odd using if-els
+
+# num=int(input("Enter a number : "))
+
+# if num%2==0:
+#     print("Even  ")
+# else :
+#     print("Odd")
+
+# 8.Input a string and print whether it contains the letter 'a' or not
+
+
+# strr=input("Enter a String : ")
+
+# if 'a' in strr:
+#     print("it contain a ")
+# else:
+#     print("it not contain a ")
+
+#9.Input a number and check if it is positive, negative, or zero
+
+# num=int(input("Enter a number : "))
+
+# if num==0:
+#     print("It is zero")
+# elif num>0:
+#     print("Its positive number ")
+# else:
+#     print("its negative number ")
+
+
+# 10.Take a string input and print the middle character (if length is odd) or middle two
+# characters (if length is even)
+
+# strr=input("enter a string : ")
+
+
+
+# if len(strr)%2!=0:
+#     print(strr[len(strr)//2])
+# else :
+#     print(strr[len(strr)//2-1],strr[len(strr)//2])
